897ab6947f94228ab79dbdea2c42524e9a2e0d3e
Hunter <shipmang8@gmail.com>
2026-02-02 02:58:11 +0900

    removed extra files while I work on the protocol

---
+++ protocol/dto/InfoDTO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lain.protocol.dto
+{
+    internal sealed class InfoDTO
+    {
+        private long Length { get; init; }
+        private byte[]? Name { get; init; } 
+        private long PieceLength { get; init; }
+        private byte[]? Pieces { get; init; }
+
+
+        private byte[]? Md5Sum { get; init; }
+        private byte[]? Sha1 { get; init; }
+        private byte[]? Sha256 { get; init; }
+
+        private string NameString => Name != null ? Encoding.UTF8.GetString(Name) : string.Empty;
+        private int PiecesCount => Pieces != null ? Pieces.Length / 20 : 0;
+        private IEnumerable<byte[]> PieceHashes
+        {
+            get
+            {
+                if (Pieces == null) yield break;
+                for (int i = 0; i < Pieces.Length; i += 20)
+                {
+                    byte[] pieceHash = new byte[20];
+                    Array.Copy(Pieces, i, pieceHash, 0, 20);
+                    yield return pieceHash;
+                }
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ protocol/dto/TorrentDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lain.protocol.dto
+{
+    internal sealed class TorrentDTO
+    {
+
+        private byte[]? Announce { get; set; }
+        private List<List<byte>>? AnnounceList { get; set; } 
+        private InfoDTO? Info { get; set; }
+
+        private byte[]? Comment { get; set; }
+        private byte[]? CreatedBy { get; set; }
+        private long? CreationDate { get; set; }
+
+        private List<byte[]>? Sources { get; set; }  
+
+
+        private DateTimeOffset? CreationDateTimeOffset => CreationDate != null ? DateTimeOffset.FromUnixTimeSeconds(CreationDate.Value) : null; 
+
+        private  string AnnounceString => Announce != null ? Encoding.UTF8.GetString(Announce) : string.Empty;  
+        private  string CommentString => Comment != null ? Encoding.UTF8.GetString(Comment) : string.Empty;
+        private  string CreatedByString => CreatedBy != null ? Encoding.UTF8.GetString(CreatedBy) : string.Empty;
+
+
+
+
+    }
+}
